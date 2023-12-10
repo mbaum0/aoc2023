d9bf51ec47f48d4169a4099acbefe918e23a0aca
Michael Baumgarten <mbaum0@pm.me>
2023-12-10 05:59:55 +0100

    d10 template

---
+++ day10/main.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import math
+import itertools
+
+input_file = "./day10/data.txt"
+
+def part1(data):
+    return data
+
+def main():
+    with open(input_file) as f:
+        data = f.read().splitlines()
+
+    p1 = part1(data)
+    print(p1)
+
+
+if __name__ == "__main__":
+    main()
