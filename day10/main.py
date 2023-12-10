f9415ba31cf5eedf3ab3c46e8207540e04cbfd41
Michael Baumgarten <mbaum0@pm.me>
2023-12-10 07:44:11 +0100

    part2

--- day10/main.py
+++ day10/main.py
@@ -13,11 +13,16 @@ RIGHT_UP_PIPE = "J"
 LEFT_UP_PIPE= "L"
 
 VALID_PIPES = [START_POINT, H_PIPE, V_PIPE, RIGHT_DOWN_PIPE, LEFT_DOWN_PIPE, RIGHT_UP_PIPE, LEFT_UP_PIPE]
+
+VALID_UP_PIPES = [START_POINT, V_PIPE, RIGHT_DOWN_PIPE, LEFT_DOWN_PIPE]
+VALID_DOWN_PIPES = [START_POINT, V_PIPE, RIGHT_UP_PIPE, LEFT_UP_PIPE]
+VALID_RIGHT_PIPES = [START_POINT, H_PIPE, RIGHT_DOWN_PIPE, RIGHT_UP_PIPE]
+VALID_LEFT_PIPES = [START_POINT, H_PIPE, LEFT_DOWN_PIPE, LEFT_UP_PIPE]
 def get_starting_point(grid):
-    for i, row in enumerate(grid):
-        for j, col in enumerate(row):
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
             if col == START_POINT:
-                return (i, j)
+                return (x, y)
     return None
 
 def get_next_point(grid, current_point, last_point):
@@ -27,23 +32,22 @@ def get_next_point(grid, current_point, last_point):
     grid_height = len(grid)
     current_pipe = grid[y][x]
 
-
-    if y > 0 and grid[y-1][x] in VALID_PIPES:
+    if y > 0 and grid[y-1][x] in VALID_UP_PIPES:
         next_up = (x, y-1)
     else:
         next_up = None
 
-    if y < grid_height -1 and grid[y+1][x] in VALID_PIPES:
+    if y < grid_height -1 and grid[y+1][x] in VALID_DOWN_PIPES:
         next_down = (x, y+1)
     else:
         next_down = None
 
-    if x > 0 and grid[y][x-1] in VALID_PIPES:
+    if x > 0 and grid[y][x-1] in VALID_LEFT_PIPES:
         next_left = (x-1, y)
     else:
         next_left = None
 
-    if x < grid_width - 1 and grid[y][x+1] in VALID_PIPES:
+    if x < grid_width - 1 and grid[y][x+1] in VALID_RIGHT_PIPES:
         next_right = (x+1, y)
     else:
         next_right = None
@@ -103,6 +107,40 @@ def get_next_point(grid, current_point, last_point):
             return None
     else:
         return None
+    
+
+def get_area(path, grid):
+    n = len(path)
+    area = 0
+    x = path[0][0]
+    y = path[0][1]
+    part = grid[y][x]
+
+    for i in range(n - 1):
+        x = path[i][0]
+        y = path[i][1]
+        part = grid[y][x]
+        area += (path[i][0] * path[i + 1][1]) 
+        area -= (path[i + 1][0] * path[i][1])
+
+    area = abs(area) - n + 1
+    area = (abs(area) / 2.0) + 1
+
+    return area
+    
+def part2(data):
+    starting_point = get_starting_point(data)
+    previous_point = starting_point
+    next_point = get_next_point(data, previous_point, None)
+    path = [starting_point, next_point]
+    while(next_point != starting_point):
+        save_point = next_point
+        next_point = get_next_point(data, next_point, previous_point)
+        previous_point = save_point
+        path.append(next_point)
+    
+    area = get_area(path, data)
+    return area
 
         
 
@@ -129,6 +167,9 @@ def main():
 
     p1 = part1(grid)
     print(p1)
+
+    p2 = part2(grid)
+    print(p2)
 
 
 if __name__ == "__main__":
