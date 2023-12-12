598f9ef9145951b7a040033808b6caf55d554d4c
Michael Baumgarten <mbaum0@pm.me>
2023-12-12 06:41:18 +0100

    day12

--- day12/main.py
+++ day12/main.py
@@ -1,21 +1,81 @@
 from dataclasses import dataclass
 import math
-import itertools
+from itertools import product
 
 input_file = "./day12/data.txt"
 
-def part1(grid):
-    return grid
+@dataclass
+class SpringRecord:
+    spring: str
+    record: list
+    combos: int
+
+def part1(springs):
+    combo_sum = 0
+    for s in springs:
+        s.combos = 0
+        for c in gen_combos(s.spring):
+            if validate_combo(c, s.record):
+                s.combos += 1
+        combo_sum += s.combos
+    return combo_sum
+
+def validate_combo(spring, record):
+    last_group_size = 0
+    record_idx = 0
+    # get groups of broken springs
+    groups = []
+    for char in spring:
+        if char == '#':
+            last_group_size += 1
+        else:
+            if last_group_size > 0:
+                groups.append(last_group_size)
+                last_group_size = 0
+
+    if last_group_size > 0:
+        groups.append(last_group_size)
+
+
+    if len(groups) != len(record):
+        return False
+    
+    for group in groups:
+        if group != record[record_idx]:
+            return False
+        record_idx += 1
+    
+    return True
+
+def gen_combos(spring):
+    possible = []
+
+    xs = [i for i, char in enumerate(spring) if char == '?']
+
+    replacements = product(['#', '.'], repeat=len(xs))
+
+    for replacement in replacements:
+        res = list(spring)
+        for idx, char in zip(xs, replacement):
+            res[idx] = char
+        yield ''.join(res)
 
 def main():
     grid = []
     with open(input_file) as f:
-        data = f.read().splitlines()
-        for i, line in enumerate(data):
-            grid.append([c for c in line])
+        rows = f.read().splitlines()
+
+    records = []
+    for r in rows:
+        a = r.split(' ')
+        #s = [c for c in a[0]]
+        r = a[1].split(',')
+        sr = [int(i) for i in r]
+        r = SpringRecord(a[0], sr, 0)
+        records.append(r)
 
 
-    p1 = part1(grid)
+    p1 = part1(records)
     print(p1)
 
     # p2 = part2(grid)
