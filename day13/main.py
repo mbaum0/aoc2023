0087474939d5b6e752163a11e07567d101b36f6d
Michael Baumgarten <mbaum0@pm.me>
2023-12-13 08:26:46 +0100

    borked

--- day13/main.py
+++ day13/main.py
@@ -15,6 +15,32 @@ def calc_points(puzzle, index0, index1):
         else:
             return 0
     return points
+
+def calc_points_smudge(puzzle, index0, index1):
+    points = index1
+    smudge_count = 0
+    while index0 > 0 and index1 < len(puzzle):
+        i0 = set(puzzle[index0])
+        i1 = set(puzzle[index1])
+        diff1 = i0.difference(i1)
+        diff2 = i1.difference(i0)
+        diff = len(diff1.union(diff2))
+        if diff > 0:
+            if smudge_count > 1 or diff > 1:
+                return 0
+            else:
+                smudge_count += 1
+                index0 -= 1
+                index1 += 1
+
+        elif puzzle[index0] == puzzle[index1]:
+            index0 -= 1
+            index1 += 1
+        else:
+            return 0
+    if smudge_count == 1:
+        return points
+    return 0
     
 def get_possible_middles(rows):
     middles = []
@@ -25,6 +51,26 @@ def get_possible_middles(rows):
             middles.append(idx)
     return middles
 
+def part2(puzzles):
+    sum = 0
+    for idx, p in enumerate(puzzles):
+        psum = 0
+        middles = get_possible_middles(p)
+        for m in middles:
+            c = calc_points_smudge(p, m, m+1)
+            if c > 0:
+                psum += c *100
+                break
+        if psum == 0:
+            rp = rotate(p)
+            middles = get_possible_middles(rp)
+            if len(middles) == 0:
+                print("BAD")
+            for m in middles:
+                c = calc_points_smudge(rp, m, m+1)
+                psum += c
+        sum += psum
+    return sum
 
 def part1(puzzles):
     sum = 0
@@ -73,6 +119,9 @@ def main():
 
     p1 = part1(puzzles)
     print(p1)
+
+    p2 = part2(puzzles)
+    print(p2)
 
 
 if __name__ == "__main__":
