c34be9b93ad5cbd90efd159ea77eca98ee453139
Michael Baumgarten <mbaum0@pm.me>
2023-12-14 06:55:20 +0100

    fix

--- day14/main.py
+++ day14/main.py
@@ -9,28 +9,48 @@ input_file = "./day14/data.txt"
 def roll(grid, direction='up'):
     dy = 0
     dx = 0
+    startY = 0
+    startX = 0
+    endY = 0
+    endX = 0
     if direction == 'up':
         dy = -1
+        endY = len(grid)
+        endX = len(grid[0])
     elif direction == 'down':
         dy = 1
+        startY = len(grid) - 1
+        endY = -1
+        endX = len(grid[0])
     elif direction == 'left':
         dx = -1
+        endY = len(grid)
+        endX = len(grid[0])
     elif direction == 'right':
         dx = 1
-    
-    for idy, y in enumerate(grid):
-        if idy == 0:
-            continue
-        for idx, x in enumerate(y):
-            if grid[idy][idx] == 'O':
-                newY = idy + dy
-                newX = idx + dx
-                while (newY >= 0 and newY < len(grid) and newX >= 0 and newX < len(grid[0]) and grid[newY][idx] == '.'):
-                    grid[newY-dy][idx-dx] = '.'
-                    grid[newY][idx] = 'O'
+        endY = len(grid)
+        startX = len(grid[0]) - 1
+        endX = -1
+
+    deltaY = dy*-1 if dy != 0 else 1
+    deltaX = dx*-1 if dx != 0 else 1
+    for y in range(startY, endY, deltaY):
+        row = grid[y]
+        for x in range(startX, endX, deltaX):
+            if grid[y][x] == 'O':
+                newY = y + dy
+                newX = x + dx
+                while (newY >= 0 and newY < len(grid) and newX >= 0 and newX < len(grid[0]) and grid[newY][newX] == '.'):
+                    grid[newY-dy][newX-dx] = '.'
+                    grid[newY][newX] = 'O'
                     newY += dy
                     newX += dx
     return grid
+
+def print_grid(grid):
+    for y in grid:
+        print(''.join(y))
+    print()
 
 def sum_load(grid):
     sum = 0
@@ -43,12 +63,16 @@ def sum_load(grid):
     return sum
 
 def part2(grid):
-    cycles = 1000000000
+    cycles = 1
     for i in range(cycles):
         grid = roll(grid, 'up')
+        print_grid(grid)
         grid = roll(grid, 'left')
+        print_grid(grid)
         grid = roll(grid, 'down')
+        print_grid(grid)
         grid = roll(grid, 'right')
+        print_grid(grid)
     sum = sum_load(grid)
     return sum
 
