19863af2c4cbfb689077b11c8c0f4d8b6287ef5c
Michael Baumgarten <mbaum0@pm.me>
2023-12-15 06:16:25 +0100

    p1

--- day15/main.py
+++ day15/main.py
@@ -7,22 +7,28 @@ import hashlib
 
 input_file = "./day15/data.txt"
 
-def part1(grid):
-    return grid
+def part1(data):
+    p1 = 0
+    for d in data:
+        cval = 0
+        for c in d:
+            cval += ord(c)
+            cval *= 17
+            cval %= 256
+        p1 += cval
+    return p1
+
+
 
 def main():
     with open(input_file) as f:
-        rows = f.read().splitlines()
+        data = f.read()
 
-    grid = []
-    for row in rows:
-        grid.append(list(row))
+    data = data.replace('\n', '')
 
-    p1 = part1(grid)
+    data = data.split(',')
+    p1 = part1(data)
     print(p1)
-
-
-
 
 if __name__ == "__main__":
     main()
